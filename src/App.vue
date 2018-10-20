ab60e7e4034c8efc7da41a83000d5613b85a5d59
Chaim Lev-Ari <chiptus@gmail.com>
2018-10-20 19:08:39 +0300

    style tabs

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,22 @@
       <input type="text" v-model="keyword" autofocus="">
     </div>
     <div class="search-results" v-if="results.length">
-      <ul>
+      <ul class="search-results--nav">
         <li v-for="result in results" 
-          v-bind:key="result.name">
-          <button v-on:click="setCurrentResult(result)">{{result.name}}</button>
+          v-bind:key="result.name"
+          v-on:click="setCurrentResult(result)"
+          class="nav-button"
+          :class="result === currentResult ? 'active' : ''">
+          {{result.name}}
         </li>
       </ul>
-      <vue-friendly-iframe v-if="keyword && currentResult" :src="currentResult.query(keyword)"></vue-friendly-iframe>
+      <div class="browser">
+        <vue-friendly-iframe 
+          v-for="result in results"
+            v-bind:key="result.name"
+            v-if="keyword"
+            v-show="currentResult == result" :src="result.query(keyword)"></vue-friendly-iframe>
+      </div>
     </div>
   </div>
 </template>
@@ -39,6 +48,10 @@ export default {
 </script>
 
 <style>
+:root {
+  --browser-border-color: #9b6f35;
+}
+
 iframe {
   height: 100%;
   width: 100%;
@@ -76,5 +89,42 @@ body {
   text-align: center;
   color: #2c3e50;
   height: 100%;
+}
+
+.search-results--nav {
+  display: flex;
+  list-style: none;
+}
+
+.search-results--nav button {
+  background: none;
+  border: none;
+}
+
+.nav-button {
+  cursor: pointer;
+  padding: 1em;
+  background-color: antiquewhite;
+  border: solid 1px var(--browser-border-color);
+  border-bottom: none;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+}
+
+.nav-button:hover {
+  background-color: #f6cb93;
+}
+
+.nav-button:active {
+  background-color: #f48e0a;
+}
+
+.nav-button.active {
+  background-color: #f4a339;
+}
+
+.browser {
+  border: solid 1px var(--browser-border-color);
+  height: calc(100% - 2.5em);
 }
 </style>
