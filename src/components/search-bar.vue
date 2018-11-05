63a14813fc76e63e08aafffa15071cfa4f835546
Chaim Lev-Ari <chiptus@gmail.com>
2018-11-05 13:03:41 +0200

    add word list (very basic)

--- src/components/search-bar.vue
+++ src/components/search-bar.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="search-bar">
+    <select v-model="selectedWord">
+      <option disabled value=""></option>
+      <option v-for="word in words" :key="word.word + word.description + word.category" :value="word">{{word.word}}</option>
+    </select>
+    <div v-if="selectedWord">{{selectedWord.category}}</div>
     <input type="text" v-model="keyword" autofocus="" 
       placeholder="Word" 
       v-on:keyup="submitOnEnter">
@@ -10,16 +15,32 @@
 </template>
 
 <script>
+import words from '../word-list.json';
+import spanishWords from '../spanish-list.json';
+
 export default {
   data() {
     return {
       keyword: this.initialKeyword,
+      words,
+      selectedWord: null,
     };
   },
   props: {
     initialKeyword: { type: String, required: true },
     onSearch: { type: Function, required: true },
     onClose: { type: Function, required: true },
+  },
+  watch: {
+    selectedWord(newValue) {
+      if (!newValue) {
+        return;
+      }
+      var spanishWord = spanishWords.find(
+        w => w.english.toLowerCase() === newValue.word.toLowerCase()
+      );
+      this.keyword = spanishWord ? spanishWord.spanish : '';
+    },
   },
   methods: {
     submitOnEnter({ keyCode }) {
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import Vue from 'vue/dist/vue.js';
+import Vue from 'vue';
 import App from './App.vue';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import {
