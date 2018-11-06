d469b8d177f645b42358a5cc0642804f4430b0bd
Chaim Lev-Ari <chiptus@gmail.com>
2018-11-06 08:46:02 +0200

    change googe images to spanish and size to large

--- src/lib/urls.js
+++ src/lib/urls.js
@@ -1,8 +1,8 @@
 export const googleImages = {
   name: 'Google Images',
   query: q =>
-    `http://www.google.es/search?q=${q}&hl=en&tbo=d&site=imghp&tbm=isch&sout=1&biw=1366&bih=643`,
-  external: true
+    `http://www.google.es/search?q=${q}&hl=es&tbs=isz:l&tbo=d&site=imghp&tbm=isch&sout=1&biw=1366&bih=643`,
+  external: true,
 };
 
 export const wiktionary = {
