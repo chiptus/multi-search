c18af6959240ee7a1f04ae6bb51f09efd1faca7d
Chaim Lev Ari <chiptus@gmail.com>
2019-02-15 11:07:23 +0200

    prevent unload

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue';
 import { store } from './lib/store';
 import initIcons from './lib/icons';
 
+//prevent unload
+window.onbeforeunload = function() {
+  return "Are you sure you want to navigate away?";
+}
+
 initIcons();
 Vue.config.productionTip = false;
 
